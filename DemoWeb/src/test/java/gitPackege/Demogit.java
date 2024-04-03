230fa42183c6c17bb644a5be525c73a66e3e0e98
soumya <soumya@DESKTOP-NT3AJV9>
2024-04-03 17:21:05 +0530

    Added 1 file

---
+++ DemoWeb/src/test/java/gitPackege/Demogit.java
@@ -0,0 +1,14 @@
+package gitPackege;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demogit {
+	
+	@Test
+	public static void start() {
+		Reporter.log("It started");
+		
+	}
+
+}
